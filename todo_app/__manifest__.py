b839872e062cdde20d178b2c53f569f295f11d48
Omar Albader <oaalbader@gmail.com>
2019-07-29 17:33:13 +0000

    init project

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,11 @@
+{
+        'name': 'To-Do Application',
+        'description': 'Manage personal to-do tasks.',
+        'author': 'Omar Albader',
+        'depends': ['base'],
+        'application': True,
+        'data': [
+            'views/todo_menu.xml',
+            'views/todo_view.xml',
+        ],
+}
